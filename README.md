fde36fc018b1e34f0ffdd6f1fa8a737ad2d494e2
nisha4545 <88261286+nisha4545@users.noreply.github.com>
2025-03-28 22:23:46 +0530

    unit test auth signup

--- README.md
+++ README.md
@@ -45,3 +45,8 @@ Will use browser as HTTP client or POSTMAN for making http requests requests to 
 3. Use stripe's secret key on server side to authenticate api calls to stripe , must be kept confidential as it has full access to account.
 4. Publishable key will be used on client side to tokenize credit card data before sending it to server
 5. Calling stripe API in test mode doesn't require card data as input
+
+#### Unit tests to check API endpoints logic
+
+1. In production environments and real systems , integration tests which make real calls to databse using a separate test databse for testing.
+2. We will override database dependencies ,so there will be no real calls to db
--- src/users/routes.py
+++ src/users/routes.py
@@ -5,36 +5,28 @@ from src.auth.dependencies import AccessTokenBearer
 from src.auth.services import UserService
 from src.users.schemas import UserProfileResponse,UserUpdateRequest
 from datetime import datetime
+from .utils import authorize_current_user
+
 
 user_service=UserService()
 
 profile_router=APIRouter()
 
-#**check for routes names and reformat 
-#check to modify get_user_details by email , which is more safe by user.id or user.email 
+
 @profile_router.get("/",response_model=UserProfileResponse)
 async def get_my_profile(jwt_token:dict=Depends(AccessTokenBearer()), db_session:AsyncSession=Depends(get_session)):
-    user_id=jwt_token["user"]["user_id"]
-    user=user_service.get_user_details(user_id,db_session)
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User not found")
-    return user
 
-
-@profile_router.patch("/{user_id}")
+    token_user_id=jwt_token["user"]["user_id"]
+    return await authorize_current_user(token_user_id,db_session)
+   
+@profile_router.patch("/")
 async def update_user_profile(
-    user_id:int,user:UserUpdateRequest,jwt_token:dict=Depends(AccessTokenBearer()),db_session:AsyncSession=Depends(get_session)):
+    user:UserUpdateRequest,jwt_token:dict=Depends(AccessTokenBearer()),db_session:AsyncSession=Depends(get_session)):
+    
+    token_user_id=jwt_token["user"]["user_id"]
+    old_user=await authorize_current_user(token_user_id,db_session)
 
-    user=jwt_token["user"]
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User not found")
-    
-    if not user["id"]==user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to update another user.")
-    
-    old_user=await user_service.get_user_details(user_id,db_session)
-    
-    update_data=user.model_dump(exclude_unset=True) # convert a model ti dict & submodels too recursively ,only include fields passed by user
+    update_data=user.model_dump(exclude_unset=True) # convert a model to dict & submodels too recursively ,only include fields passed by user
 
     for field,value in update_data.items():
         setattr(old_user,field,value)
@@ -44,27 +36,17 @@ async def update_user_profile(
     return old_user  
 
 
-@profile_router.delete("/{user_id}")
-async def delete_user(user_id:int,jwt_token:dict=Depends(AccessTokenBearer()),db_session:AsyncSession=Depends(get_session)):
-    user_id=jwt_token["user"]["user_id"]
-    user=user_service.get_user_details(user_id,db_session)
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User not found")
-    
-    if not user["id"]==user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to update another user.")
+@profile_router.delete("/")
+async def delete_user(jwt_token:dict=Depends(AccessTokenBearer()),db_session:AsyncSession=Depends(get_session)):
+    token_user_id=jwt_token["user"]["user_id"]
+    cur_user=await authorize_current_user(token_user_id,db_session)
     
     #*add password enter check 
     
-    user.deleted_at=datetime.now()
+    cur_user.deleted_at=datetime.now()
     await db_session.commit()
     
     return {"message": "User deleted successfully"}
-
-
-    
-
-
 
 
 
